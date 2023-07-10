d58043ac5d38a96b6830d28297674cabe60185af
MinhQuy <onequy@gmail.com>
2023-07-10 17:08:39 +0700

    Update Firebase.js

--- Src/Common/Firebase/Firebase.js
+++ Src/Common/Firebase/Firebase.js
@@ -10,6 +10,18 @@ import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = null; // add config here!
 
+// Examble config:
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAKOAaryHgVZ-ImHz8Z2iFdz8Ex57LY",
+//     authDomain: "tictactoe-d1111.firebaseapp.com",
+//     databaseURL: "https://tictactoe-d3339-default-rtdb.asia-southeast1.firebasedatabase.app",
+//     projectId: "tictactoe-d4112",
+//     storageBucket: "tictactoe-d4333.appspot.com",
+//     messagingSenderId: "424601343292",
+//     appId: "1:424601817692:web:491c78d23fd2df4c2abcc99"
+// };
+
 var FirebaseApp = null;
 
 export function GetFirebaseApp() {
@@ -17,12 +29,12 @@ export function GetFirebaseApp() {
     return FirebaseApp;
 }
 
-export function FirebaseInit() {    
+export function FirebaseInit() {
     if (FirebaseApp)
         return;
 
     if (firebaseConfig === null)
-        throw 'You should update the firebaseConfig to the file: Src\Common\Firebase\Firebase.js first!';
+        throw 'You should update the firebaseConfig to the file: Src\\Common\\Firebase\\Firebase.js first!';
 
     FirebaseApp = initializeApp(firebaseConfig);
 }
